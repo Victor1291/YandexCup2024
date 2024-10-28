3a5ef70cdee84bca7a7a1080aea138d0ae8502ce
Dmitrii Maksimov <dmitrij.maksimow@gmail.com>
2024-10-28 14:10:23 +0300

    Add Hilt

--- app/src/main/java/ru/kartollika/yandexcup/MainActivity.kt
+++ app/src/main/java/ru/kartollika/yandexcup/MainActivity.kt
@@ -6,14 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Modifier
+import dagger.hilt.android.AndroidEntryPoint
 import ru.kartollika.yandexcup.canvas.CanvasScreen
 import ru.kartollika.yandexcup.ui.theme.YandexCup2024Theme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     enableEdgeToEdge()
-    
+
     setContent {
       YandexCup2024Theme {
         CanvasScreen(
---
+++ app/src/main/java/ru/kartollika/yandexcup/canvas/CanvasViewModel.kt
@@ -0,0 +1,9 @@
+package ru.kartollika.yandexcup.canvas
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class CanvasViewModel @Inject constructor() : ViewModel() {
+}
